d95ba3daf72af1c8adf6e16170de777a6cccab9f
naturenomad <76237641+naturenomad@users.noreply.github.com>
2021-02-26 13:32:06 +0100

    Create textwrap.py

---
+++ textwrap.py
@@ -0,0 +1,25 @@
+# TEXT WRAP
+
+# Given a string S and width W .
+# Task is to wrap the string into a paragraph of width W.
+
+def wrap(string, max_width):
+    segments = ''
+    segment = ''
+    for i in range(0,len(string)):
+        if len(segment) == max_width:
+            segments+='\n'
+            segment = ''
+        segment += string[i]
+        segments += string[i]
+    
+    return(segments)
+
+# Call internally
+#print(wrap('ABCDEFGHIJKLIMNOQRSTUVWXYZ', 4))
+
+# Call externally
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)

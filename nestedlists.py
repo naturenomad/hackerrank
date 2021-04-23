3afe4cb3a1bd304d4c4a64d4c1d502b20748eec1
naturenomad <76237641+naturenomad@users.noreply.github.com>
2021-04-23 14:01:28 +0200

    Added source page

--- nestedlists.py
+++ nestedlists.py
@@ -29,7 +29,7 @@ def nested(players):
 
 
 # Calling internally
-nested([['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]])
+# nested([['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]])
 # Expect :
 # Berry
 # Harry

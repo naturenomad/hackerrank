7da3c2a6f360891c83c3d6f3ac50e3050b11251e
naturenomad <76237641+naturenomad@users.noreply.github.com>
2021-05-06 03:54:51 +0200

    Source page added

--- jumpingontheclouds.py
+++ jumpingontheclouds.py
@@ -1,3 +1,6 @@
+
+# https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
+
 # JUMPING ON THE CLOUDS
 
 # There is a new mobile game that starts with consecutively numbered clouds. Some of the clouds are thunderheads 

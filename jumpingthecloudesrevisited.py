ea9afe4e751c63fa24c37dd3ac2f5b3a8e1b75ec
naturenomad <76237641+naturenomad@users.noreply.github.com>
2022-04-17 11:32:39 +1200

    Update jumpingthecloudesrevisited.py

--- jumpingthecloudesrevisited.py
+++ jumpingthecloudesrevisited.py
@@ -4,7 +4,8 @@
 
 # https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited
 
-# A child is playing a cloud hopping game. In this game, there are sequentially numbered clouds that can be thunderheads or cumulus clouds. The character must jump from cloud to cloud until it reaches the start again. 
+# A child is playing a cloud hopping game. In this game, there are sequentially numbered clouds that can be thunderheads or cumulus clouds. 
+# The character must jump from cloud to cloud until it reaches the start again. 
 
 # Energy level e starts at 100
 # Cumulus cloud = 0

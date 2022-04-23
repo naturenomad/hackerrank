604f00ff50b7fd1b6fc49f301e4499295343c267
naturenomad <76237641+naturenomad@users.noreply.github.com>
2022-04-23 13:21:10 +1200

    Create basic.sql

---
+++ basic.sql
@@ -0,0 +1,9 @@
+
+-- Test of the hackerrank SQL basic queries :
+
+-- https://www.hackerrank.com/domains/sql
+-- https://www.hackerrank.com/domains/sql?filters%5Bskills%5D%5B%5D=SQL%20%28Basic%29
+
+-- https://www.hackerrank.com/challenges/select-by-id/
+-- Query all columns for a city in CITY with the ID 1661.
+select * from city where id=1661;
